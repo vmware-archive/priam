79ab522a8940c24abb8b668e2269d3eafea0d314
Dale Olds <olds@vmware.com>
2015-03-01 13:21:58 -0800

    save progress on manual yaml handling

--- wks.go
+++ wks.go
@@ -4,18 +4,92 @@ import (
 	//"golang.org/x/oauth2"
 	"fmt"
 	"github.com/codegangsta/cli"
-	yaml "github.com/smallfish/simpleyaml"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
-    "path/filepath"
 )
 
-func boom(c *cli.Context) {
-	println("command was run with no params... print help here?")
+type target struct {
+	Name         string
+	ClientID     string
+	ClientSecret string
+	Host         string
+	Current      bool
 }
 
-func health(c *cli.Context) {
+var appConfig struct {
+	DebugMode bool
+	Targets   []target
+}
+
+var currentTarget uint
+var appConfigFile string = "./wks.yaml"
+
+type wksAppConfig struct {
+	packageVersion string
+	description    string
+	iconFile       string
+	attributeMaps  map[string]string
+	accessPolicy   string
+	authInfo       map[string]string
+}
+
+type manifest struct {
+	name      string
+	memory    string
+	instances int
+	path      string
+	buildpack string
+	env       map[string]string
+	workspace wksAppConfig
+}
+
+func getAppConfig() {
+	getYAML("./wks.yaml", &appConfig)
+	/*
+		var cfg map[interface{}]interface{}
+		getYAML("./wks.yaml", &cfg)
+		for _, av := range cfg["targets"].([]interface{}) {
+			t := av.(map[interface{}]interface{})
+			//fmt.Printf("t %#v\n", t)
+			tgt := target{ystr(t["name"]), ystr(t["clientid"]),
+				ystr(t["clientsecret"]), ystr(t["host"]),
+				ybool(t["current"])}
+			//fmt.Printf("target %#v\n", tgt)
+			targets = append(targets, tgt)
+		}
+		//fmt.Printf("targets %#v\n", targets)
+	*/
+}
+
+func putAppConfig() {
+	putYAML("./wks_out.yaml", appConfig)
+}
+
+func getManifest() {
+	var cfg map[interface{}]interface{}
+	getYAML("./manifest.yaml", &cfg)
+	fmt.Printf("\n\nAppValue: %#v\n\n", cfg["applications"])
+	for _, app := range cfg["applications"].([]interface{}) {
+		for k, v := range app.(map[interface{}]interface{}) {
+			fmt.Printf("key %v, Value: %#v\n\n", k, v)
+		}
+	}
+}
+
+func cmdPush(c *cli.Context) {
+	println("push app command")
+	getManifest()
+}
+
+func cmdTarget(c *cli.Context) {
+	//fmt.Printf()
+	println("target command")
+}
+
+func cmdHealth(c *cli.Context) {
+	println("health:\n\n")
 	resp, err := http.Get("https://radio.workspaceair.com/SAAS/jersey/manager/api/health")
 	if err != nil {
 		fmt.Printf("Error on GET: %v\n", err)
@@ -30,69 +104,42 @@ func health(c *cli.Context) {
 	fmt.Printf("Body: %v\n", string(body))
 }
 
+func wks(args []string, inCfg []bytes, outCfg []bytes, out ioutil.Writer, error ioutl.Writer) (error uint) {
+
+}
 
 func main() {
 	app := cli.NewApp()
 	app.Name = "wks"
 	app.Usage = "general usage goes here"
-	app.Action = boom
-
-	filename, _ := filepath.Abs("./manifest.yaml")
-	yamlFile, err := ioutil.ReadFile(filename)
-	if err != nil {
-		panic(err)
-	}
-
-	y, err := yaml.NewYaml(yamlFile)
-	if err != nil {
-		panic(err)
-	}
-
-	applications, err := y.Get("applications").Array()
-	if err != nil {
-		panic(err)
-	}
-
-	for i, _ := range applications {
-		fmt.Printf("index: %v, value: %#v\n", i, y.Get("applications").GetIndex(i).Get("name"))
-	}
-
-	//println(y.Get("applications").GetIndex(0).Get("name").String())
-	//config := make(map[string]interface{})
-
-	//err = yaml.Unmarshal(yamlFile, &config)
-	//if err != nil {
-	//	panic(err)
-	//}
-
-	//fmt.Printf("Value: %#v\n\n", config)
-	//fmt.Printf("Value: %#v\n", config["applications"])
-
-	//appdata := make([]map[string]interface{})
-	//apps := config["applications"].([]interface{})
-	//app0 := apps[0].(map[interface{}]interface{})
-	//app0s := app0.(map[string]interface{})
-	//println(app0["name"].(string))
-	//println(app0["name"])
-
-	//fmt.Printf("Value: %#v\n", config["applications"])
-    //for k := range config {
-    //    println("key: ", k)
-    //}
+	app.Action = cli.ShowAppHelp
+	app.Email = ""
+	app.Author = ""
 
 	app.Commands = []cli.Command{
 		{
 			Name:      "health",
 			ShortName: "h",
 			Usage:     "check workspace service health",
-			Action:    health,
+			Action:    cmdHealth,
 		},
 		{
-			Name:      "complete",
-			ShortName: "c",
-			Usage:     "complete a task on the list",
-			Action: func(c *cli.Context) {
-				println("completed task: ", c.Args().First())
+			Name:      "push",
+			ShortName: "p",
+			Usage:     "push an application",
+			Action:    cmdPush,
+		},
+		{
+			Name:      "target",
+			ShortName: "t",
+			Usage:     "set or display the target workspace instance",
+			Action:    cmdTarget,
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "force, f",
+					Value: "",
+					Usage: "force target even if workspace instance not reachable",
+				},
 			},
 		},
 		{
@@ -118,5 +165,12 @@ func main() {
 		},
 	}
 
+	getAppConfig()
+	putAppConfig()
+	url, err := url.Parse("what.com/")
+	fmt.Printf("url: %#v, err: %v", url, err)
+
 	app.Run(os.Args)
 }
+
+
