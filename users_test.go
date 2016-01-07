f783b33176e8637ceb4f8200d2fdbf8e82bec720
Dale Olds <olds@vmware.com>
2016-01-07 15:27:18 -0800

    Fixed bug in which name wasn't displayed when setting password
    Also ensured that the name is taken from the executable
    Added unit tests and test helpers
    Increased test coverage from 33 to 35%

---
+++ users_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestSetPassword(t *testing.T) {
+	pwdH := func(t *testing.T, req *tstReq) *tstReply {
+		assert.Contains(t, req.input, "Password")
+		return &tstReply{status: 204}
+	}
+	idH := func(t *testing.T, req *tstReq) *tstReply {
+		output := `{"resources": [{ "userName" : "john", "id": "12345"}]}`
+		return &tstReply{output: output, contentType: "application/json"}
+	}
+	srv := StartTstServer(t, map[string]tstHandler{
+		"POST/scim/Users/12345": pwdH,
+		"GET/scim/Users":        idH})
+	ctx := newHttpContext(newBufferedLogr(), srv.URL, "/", "")
+	ctx.log.traceOn = true
+	cmdSetPassword(ctx, "john", "travolta")
+	assert.Empty(t, ctx.log.errString())
+	assert.Contains(t, ctx.log.infoString(), `User "john" updated`)
+}
